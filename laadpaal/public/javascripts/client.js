54fc89dddfbccf51fbf3d07cbbf9061616e9d54b
JimvandeVen <Jim.van.de.Ven@hva.nl>
2019-06-04 15:21:24 +0200

    finaly made the complete sentence on the complaint screen html/css/js

--- laadpaal/public/javascripts/client.js
+++ laadpaal/public/javascripts/client.js
@@ -1,6 +1,6 @@
 console.log('client.js loaded.')
 const complaintForm = document.querySelector(".complaintForm")
-const complaintButtons = document.querySelectorAll(".complaintButton")
+const complaintButtons = document.querySelectorAll(".option")
 const selectedComplaint = document.querySelector(".selectedComplaint")
 
 if (complaintButtons) {
@@ -11,54 +11,99 @@ if (complaintButtons) {
             const targetElement = selectedComplaint
             const movingElement = e.srcElement
 
-            moveElement(targetElement, movingElement)
-            transitionElement(complaintButtons, movingElement)
+            const sentenceStart = e.srcElement.innerHTML
 
-            console.log(targetElement, movingElement)
+            // moveElement(targetElement, movingElement)
+            createSentence(targetElement, sentenceStart)
+            slideElementOut(complaintButtons, movingElement)
         })
     })
 }
 
 function moveElement(targetElement, movingElement) {
     targetElement.appendChild(movingElement);
-    console.log(movingElement)
+    movingElement.classList.remove("option")
 }
 
-function transitionElement(complaintButtons, movingElement) {
+function createSentence(targetElement, sentenceStart) {
+    targetElement.innerHTML = sentenceStart;
+    // movingElement.classList.remove("option")
+}
+
+
+function slideElementOut(complaintButtons, movingElement) {
     complaintButtons.forEach(complaintButton => {
         const chosenType = movingElement.innerHTML
         if (complaintButton.innerHTML != chosenType) {
-            complaintButton.classList.add("slideRight")
+            complaintButton.classList.add("slideOut")
             setTimeout(function () {
                 addNextAnswers(chosenType)
             }, 500);
         }
-
     })
-
 }
 
 function addNextAnswers(chosenType) {
-
     let newChild
     if (chosenType == "De laadpaal...") {
         newChild = `
-    <button class="complaintButton slideLeft">...is uit.</button>
-    <button class="complaintButton slideLeft">...is defect.</button>
-    <button class="complaintButton slideLeft">...is te lang bezet.</button>
-    <button class="complaintButton slideLeft">...is niet te bereiken.</button>
-    <button class="complaintButton slideLeft">...is bezet door een niet elektrische.</button>`
+    <button class="complaintButton option slideIn">...is uit.</button>
+    <button class="complaintButton option slideIn">...is defect.</button>
+    <button class="complaintButton option slideIn">...is te lang bezet.</button>
+    <button class="complaintButton option slideIn">...is niet te bereiken.</button>
+    <button class="complaintButton option slideIn">...is bezet door een niet elektrische auto.</button>`
     } else if (chosenType == "De parkeerplek...") {
         newChild = `
-    <button class="complaintButton slideLeft">...is te lang bezet.</button>
-    <button class="complaintButton slideLeft">...is niet te bereiken.</button>
-    <button class="complaintButton slideLeft">...is bezet door een niet elektrische.</button>`
+    <button class="complaintButton option slideIn">...is te lang bezet.</button>
+    <button class="complaintButton option slideIn">...is niet te bereiken.</button>
+    <button class="complaintButton option slideIn">...is bezet door een niet elektrische auto.</button>`
     } else if (chosenType == "Mijn pasje...") {
         newChild = `
-    <button class="complaintButton slideLeft">...werkt niet.</button>
-    <button class="complaintButton slideLeft">...is kapot.</button>
-    <button class="complaintButton slideLeft">...heb ik niet bij me.</button>`
+    <button class="complaintButton option slideIn">...werkt niet.</button>
+    <button class="complaintButton option slideIn">...is kapot.</button>
+    <button class="complaintButton option slideIn">...heb ik niet bij me.</button>`
     }
 
     complaintForm.innerHTML = newChild
+    const newComplaintButtons = document.querySelectorAll(".option")
+
+    addEventListeners(newComplaintButtons)
+    removeAnimation(newComplaintButtons)
+}
+
+function removeAnimation(newComplaintButtons) {
+    setTimeout(function () {
+        newComplaintButtons.forEach(newComplaintButton => {
+            newComplaintButton.classList.remove("slideIn")
+        })
+    }, 1000);
+}
+
+function addEventListeners(newComplaintButtons) {
+    newComplaintButtons.forEach(newComplaintButton => {
+        newComplaintButton.addEventListener("click", e => {
+            e.preventDefault()
+
+            const sentenceStart = selectedComplaint.innerHTML
+            const movingElement = e.srcElement
+
+            const sentenceEnd = e.srcElement.innerHTML
+            console.log(sentenceStart);
+
+            completeSentence(sentenceStart, sentenceEnd)
+            slideElementOut(newComplaintButtons, movingElement)
+        })
+    })
+}
+
+function completeSentence(sentenceStart, sentenceEnd) {
+    let firstPart = sentenceStart
+    let secondPart = sentenceEnd
+
+    let fullSentence = []
+
+    fullSentence.push(firstPart.substring(0, firstPart.length - 3))
+    fullSentence.push(secondPart.substring(3, secondPart.length))
+
+    selectedComplaint.innerHTML = fullSentence.join(" ")
 }
