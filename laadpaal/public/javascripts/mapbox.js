ab9391a83b7306ddf30bab50ec96f8ca575a256c
Robin Hofman <roobin1999@gmail.com>
2019-06-14 13:07:37 +0200

    pointers have their own color now

--- laadpaal/public/javascripts/mapbox.js
+++ laadpaal/public/javascripts/mapbox.js
@@ -1,28 +1,54 @@
-
 // MapBox
-mapboxgl.accessToken = 'pk.eyJ1Ijoicm9vYmluMTk5OSIsImEiOiJjanJxYzVpeGIwdzJ4NDlycTZvd2lramRkIn0.jEoxjM-oE38jYCIHnhLw_g';
- 
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoicm9vYmluMTk5OSIsImEiOiJjanJxYzVpeGIwdzJ4NDlycTZvd2lramRkIn0.jEoxjM-oE38jYCIHnhLw_g";
+
 // hva location
-var mapLong = '4.909203';
-var mapLat = '52.360157';
+var mapLong = "4.909203";
+var mapLat = "52.360157";
+var complaints = [];
 
 //create map
 var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11?optimize=true",
-    zoom: 15,
-    center: [mapLong, mapLat]
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11?optimize=true",
+  zoom: 15,
+  center: [mapLong, mapLat]
 });
 
 // set user location
-map.addControl(new this.mapboxgl.GeolocateControl({
-  positionOptions: {
-    enableHighAccuracy: true
-  },
-  trackUserLocation: true
-}))
+map.addControl(
+  new this.mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true
+    },
+    trackUserLocation: true
+  })
+);
 
-const query = ` 
+
+var query =  `
+query {
+  complaints {
+    pole {
+      _id
+    }
+  }
+}
+`;
+
+fetch("/graphql", {
+  method: "POST",
+  headers: { "content-type": "application/json" },
+  body: JSON.stringify({ query })
+})
+  .then(response => response.json())
+  .then(data => {
+    data.data.complaints.forEach(complaint => {
+      complaints.push(complaint.pole._id)
+    })
+  })
+
+query = ` 
 query {
   poles {
     _id
@@ -39,46 +65,60 @@ query {
     sockets
     usedsockets
   }
-} `
+} `;
 
-fetch('/graphql', {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ query }),
-}).then(response => response.json())
-    .then(data => {
+fetch("/graphql", {
+  method: "POST",
+  headers: { "content-type": "application/json" },
+  body: JSON.stringify({ query })
+})
+  .then(response => response.json())
+  .then(data => {
 
-          for(var i = 0; i < data.data.poles.length; i++ ) {
+    console.log(data);
 
-            pole = data.data.poles[i];
+    data.data.poles.forEach(pole => {
 
-            var pointer = {
-              type: 'FeatureCollection',
-              features: [{
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [pole.longitude, pole.latitude]
-                },
-                properties: {
-                  title: pole.address,
-                }
-              }]
+      var pointer = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [pole.longitude, pole.latitude]
+            },
+            properties: {
+              title: pole.address
             }
+          }
+        ]
+      };
 
-            var title = pole.address;
-            var id = pole._id;
-            
-            var el = document.createElement('div')
-            el.className = 'marker';
+      var title = pole.address;
+      var id = pole._id;
 
-            new mapboxgl.Marker(el)
-                .setLngLat(pointer['features'][0]['geometry']['coordinates'])
-                .setPopup(new mapboxgl.Popup({ offset: 15 }) // add popups
-                .setHTML(`
-                          <h3>${title}</h3>
-                          <p><a href="/setpole/${id}">Klacht indienen</a></p>
-                          <p><a href="/complaint/details/${id}">Klachten bekijken</a></p>
-                          `))
-                .addTo(map);
-        }});
+      var el = document.createElement("div");
+
+      // set point color
+      if(complaints.includes(id)) { // if pole has a complaint
+        el.className = "marker yellow"; 
+      } else {
+        if(pole.usedsockets == pole.sockets) { // both poles in use
+          el.className = "marker red";  
+        } else { // no complaint + open spot
+          el.className = "marker green"
+        }
+      }
+
+      new mapboxgl.Marker(el)
+          .setLngLat(pointer['features'][0]['geometry']['coordinates'])
+          .setPopup(new mapboxgl.Popup({ offset: 15 }) // add popups
+          .setHTML(`
+                    <h3>${title}</h3>
+                    <p><a href="/setpole/${id}">Klacht indienen</a></p>
+                    <p><a href="/complaint/details/${id}">Klachten bekijken</a></p>
+                    `))
+          .addTo(map);
+    });
+  });
