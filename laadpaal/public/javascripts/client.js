0564230670e5384dd68301e237be2fdd9a88ca4c
JimvandeVen <Jim.van.de.Ven@hva.nl>
2019-06-21 13:45:05 +0200

    updated chat css

--- laadpaal/public/javascripts/client.js
+++ laadpaal/public/javascripts/client.js
@@ -8,121 +8,121 @@ const subtitle = document.querySelector(".subtitle");
 const myStorage = window.localStorage;
 
 if (complaintButtons) {
-  complaintButtons.forEach(complaintButton => {
-    complaintButton.addEventListener("click", e => {
-      e.preventDefault();
+    complaintButtons.forEach(complaintButton => {
+        complaintButton.addEventListener("click", e => {
+            e.preventDefault();
 
-      const targetElement = selectedComplaint;
-      const movingElement = e.srcElement;
-      const sentenceStart = e.srcElement.innerHTML;
+            const targetElement = selectedComplaint;
+            const movingElement = e.srcElement;
+            const sentenceStart = e.srcElement.innerHTML;
 
-      createSentence(targetElement, sentenceStart);
-      slideElementOut(complaintButtons, movingElement);
+            createSentence(targetElement, sentenceStart);
+            slideElementOut(complaintButtons, movingElement);
+        });
     });
-  });
 }
 
 function moveElement(targetElement, movingElement) {
-  targetElement.appendChild(movingElement);
-  movingElement.classList.remove("option");
+    targetElement.appendChild(movingElement);
+    movingElement.classList.remove("option");
 }
 
 function createSentence(targetElement, sentenceStart) {
-  targetElement.innerHTML = sentenceStart;
+    targetElement.innerHTML = sentenceStart;
 }
 
 function slideElementOut(complaintButtons, movingElement) {
-  complaintButtons.forEach(complaintButton => {
-    const chosenType = movingElement.innerHTML;
-    if (complaintButton.innerHTML != chosenType) {
-      complaintButton.classList.add("slideOut");
-      setTimeout(function() {
-        addNextAnswers(chosenType);
-      }, 500);
-    }
-  });
+    complaintButtons.forEach(complaintButton => {
+        const chosenType = movingElement.innerHTML;
+        if (complaintButton.innerHTML != chosenType) {
+            complaintButton.classList.add("slideOut");
+            setTimeout(function() {
+                addNextAnswers(chosenType);
+            }, 500);
+        }
+    });
 }
 
 function addNextAnswers(chosenType) {
-  let newChild;
-  if (chosenType == "De laadpaal...") {
-    newChild = `
+    let newChild;
+    if (chosenType == "De laadpaal...") {
+        newChild = `
     <button class="complaintButton option slideIn">...is uit/defect.</button>
     <button class="complaintButton option slideIn">...is te lang bezet.</button>
     <button class="complaintButton option slideIn">...is niet te bereiken.</button>
     <button class="complaintButton option slideIn">...kabel is kapot.</button>
     <button class="complaintButton option slideIn">...is bezet door een niet elektrische auto.</button>`;
-  } else if (chosenType == "De parkeerplek...") {
-    newChild = `
+    } else if (chosenType == "De parkeerplek...") {
+        newChild = `
     <button class="complaintButton option slideIn">...is te lang bezet.</button>
     <button class="complaintButton option slideIn">...is niet te bereiken.</button>
     <button class="complaintButton option slideIn">...is bezet door een niet elektrische auto.</button>`;
-  } else if (chosenType == "Mijn pasje...") {
-    newChild = `
+    } else if (chosenType == "Mijn pasje...") {
+        newChild = `
     <button class="complaintButton option slideIn">...werkt niet.</button>
     <button class="complaintButton option slideIn">...is kapot.</button>
     <button class="complaintButton option slideIn">...heb ik niet bij me.</button>`;
-  }
+    }
 
-  complaintForm.innerHTML = newChild;
-  const newComplaintButtons = document.querySelectorAll(".option");
+    complaintForm.innerHTML = newChild;
+    const newComplaintButtons = document.querySelectorAll(".option");
 
-  addEventListeners(newComplaintButtons);
-  removeAnimation(newComplaintButtons);
+    addEventListeners(newComplaintButtons);
+    removeAnimation(newComplaintButtons);
 }
 
 function removeAnimation(newComplaintButtons) {
-  setTimeout(function() {
-    newComplaintButtons.forEach(newComplaintButton => {
-      newComplaintButton.classList.remove("slideIn");
-    });
-  }, 1000);
+    setTimeout(function() {
+        newComplaintButtons.forEach(newComplaintButton => {
+            newComplaintButton.classList.remove("slideIn");
+        });
+    }, 1000);
 }
 
 function addEventListeners(newComplaintButtons) {
-  newComplaintButtons.forEach(newComplaintButton => {
-    newComplaintButton.addEventListener("click", e => {
-      e.preventDefault();
+    newComplaintButtons.forEach(newComplaintButton => {
+        newComplaintButton.addEventListener("click", e => {
+            e.preventDefault();
 
-      const sentenceStart = selectedComplaint.innerHTML;
-      const movingElement = e.srcElement;
+            const sentenceStart = selectedComplaint.innerHTML;
+            const movingElement = e.srcElement;
 
-      const sentenceEnd = e.srcElement.innerHTML;
-      e.srcElement.remove();
+            const sentenceEnd = e.srcElement.innerHTML;
+            e.srcElement.remove();
 
-      completeSentence(sentenceStart, sentenceEnd);
-      slideSecondQuestionsOut(newComplaintButtons, movingElement);
+            completeSentence(sentenceStart, sentenceEnd);
+            slideSecondQuestionsOut(newComplaintButtons, movingElement);
+        });
     });
-  });
 }
 
 function slideSecondQuestionsOut(complaintButtons, movingElement) {
-  complaintButtons.forEach(complaintButton => {
-    const chosenType = movingElement.innerHTML;
-    if (complaintButton.innerHTML != chosenType) {
-      complaintButton.classList.add("slideOut");
-      setTimeout(function() {
-        slideFormIn();
-      }, 500);
-    }
-  });
+    complaintButtons.forEach(complaintButton => {
+        const chosenType = movingElement.innerHTML;
+        if (complaintButton.innerHTML != chosenType) {
+            complaintButton.classList.add("slideOut");
+            setTimeout(function() {
+                slideFormIn();
+            }, 500);
+        }
+    });
 }
 
 function completeSentence(sentenceStart, sentenceEnd) {
-  let firstPart = sentenceStart;
-  let secondPart = sentenceEnd;
+    let firstPart = sentenceStart;
+    let secondPart = sentenceEnd;
 
-  let fullSentence = [];
+    let fullSentence = [];
 
-  fullSentence.push(firstPart.substring(0, firstPart.length - 3));
-  fullSentence.push(secondPart.substring(3, secondPart.length));
+    fullSentence.push(firstPart.substring(0, firstPart.length - 3));
+    fullSentence.push(secondPart.substring(3, secondPart.length));
 
-  selectedComplaint.innerHTML = fullSentence.join(" ");
-  myStorage.setItem("type", fullSentence.join(" "));
+    selectedComplaint.innerHTML = fullSentence.join(" ");
+    myStorage.setItem("type", fullSentence.join(" "));
 }
 
 function slideFormIn() {
-  newChild = `
+    newChild = `
     <label class="fotoButton slideIn" for="file-upload">FOTO</label>
 
     <input class="invisible" id="file-upload" name="image" type="file" class="slideIn" onchange="readURL(this)" accept="image/jpeg">
@@ -138,22 +138,22 @@ function slideFormIn() {
        <button class="button formButtonRight  slideIn" type="submit">GA VERDER</button>
     </div>
     `;
-  subtitle.innerHTML = "Voeg eventueel een foto of beschrijving toe";
-  complaintForm.innerHTML = newChild;
+    subtitle.innerHTML = "Voeg eventueel een foto of beschrijving toe";
+    complaintForm.innerHTML = newChild;
 }
 
 function readURL(input) {
-  if (input.files && input.files[0]) {
-    let reader = new FileReader();
-    let uploadImg = document.querySelector("#uploadImg");
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        let uploadImg = document.querySelector("#uploadImg");
 
-    reader.onload = function(e) {
-      uploadImg.setAttribute("src", e.target.result);
-      // .attr('src', e.target.result)
-      // .width(150)
-      // .height(200);
-    };
+        reader.onload = function(e) {
+            uploadImg.setAttribute("src", e.target.result);
+            // .attr('src', e.target.result)
+            // .width(150)
+            // .height(200);
+        };
 
-    reader.readAsDataURL(input.files[0]);
-  }
+        reader.readAsDataURL(input.files[0]);
+    }
 }
